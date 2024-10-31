6842c696003bad3efff5d0ec34dfe54152b6a16f
akshayamaram <akshayamaramjesus@gmail.com>
2024-10-31 19:14:19 +0530

    fix: username not being displayed in dashboards

--- src/components/HelperComponents/Header.jsx
+++ src/components/HelperComponents/Header.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Header = (props) => {
-
   const handleLogout = () => {
-    localStorage.setItem('loggedInUser', '')
-    props.changeUser('')
+    localStorage.setItem("loggedInUser", "");
+    props.changeUser("");
     // window.location.reload()
-  }
+  };
 
   return (
-    <div className='flex items-end justify-between'>
-      <h3 className='text-2xl'>Hello , <br /> <span className='font-bold text-3xl'>username 👋</span></h3>
-      <button className='px-8 py-2 bg-red-400 rounded-sm custom-shadow-logout font-medium cursor-pointer' onClick={handleLogout}>Log Out</button>
+    <div className="flex items-end justify-between">
+      <h3 className="text-2xl">
+        Hello , <br />{" "}
+        <span className="font-bold text-3xl">
+          {props.data ? props.data.firstName : "Admin"} 👋
+        </span>
+      </h3>
+      <button
+        className="px-8 py-2 bg-red-400 rounded-sm custom-shadow-logout font-medium cursor-pointer"
+        onClick={handleLogout}
+      >
+        Log Out
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header 
+export default Header;
